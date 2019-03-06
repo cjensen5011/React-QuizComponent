acee7bb55baa68eac986a02e85d09ae6c7d54c9e
christopherjensen <christopher.jensen@daugherty.com>
2019-03-06 15:32:34 -0600

    displaying quiz questions and handling events

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,24 +1,40 @@
-import React, { Component } from 'react'
-import QuizQuestion from './QuizQuestion.js'
-import QuizEnd from './QuizEnd.js'
+import React, { Component } from 'react';
+import QuizQuestion from './QuizQuestion.js';
+import QuizEnd from './QuizEnd.js';
 
-let quizData = require('./quiz_data.json')
+let quizData = require('./quiz_data.json');
 
 class Quiz extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {quiz_position: 1};
-    } 
+  constructor(props) {
+    super(props);
+    this.state = { quiz_position: 1 };
+  }
+
+  showNextQuestion() {
+    this.setState(state => {
+      return { quiz_position: state.quiz_position + 1 };
+    });
+  }
+
   render() {
-    const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length)
-    console.log(isQuizEnd)
+    const isQuizEnd =
+      this.state.quiz_position - 1 === quizData.quiz_questions.length;
+    console.log(isQuizEnd);
     return (
       <div>
-        {isQuizEnd ? <QuizEnd /> : 
-        <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} /> }
+        {isQuizEnd ? (
+          <QuizEnd />
+        ) : (
+          <QuizQuestion
+            showNextQuestionHandler={this.showNextQuestion.bind(this)}
+            quiz_question={
+              quizData.quiz_questions[this.state.quiz_position - 1]
+            }
+          />
+        )}
       </div>
-    )
+    );
   }
 }
 
-export default Quiz
+export default Quiz;
